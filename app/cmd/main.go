f30f2cef5693322d5f497755da0d63f74fe9dcc4
supermarine1377 <supermarine@outlook.jp>
2024-10-26 20:48:33 +0900

    Use os.Exit(1) instead of panic() when the app fails to run

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -11,14 +11,13 @@ import (
 
 func main() {
 	ctx, stop := signal.NotifyContext(
-context.Background(),
+		context.Background(),
 		os.Interrupt,
 		syscall.SIGTERM,
 	)
 	defer stop()
 
 	if err := app.Run(ctx); err != nil {
-		err = fmt.Errorf("failed to run server: %w", err)
-		panic(err)
+		os.Exit(1)
 	}
 }
