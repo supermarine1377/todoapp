0692ccc12104c14a503365c707cf97ba08adce5e
supermarine1377 <53213350+supermarine1377@users.noreply.github.com>
2024-10-26 20:57:09 +0900

    Handle interrupution and sigterm signals in server (#2)
    * Handle interruption and systerm signals
    
    * Add logging when server is shutdown
    
    * Use os.Exit(1) instead of panic() when the app fails to run

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/supermarine1377/todoapp/app"
 )
 
 func main() {
-	ctx := context.Background()
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
 	if err := app.Run(ctx); err != nil {
-		err = fmt.Errorf("failed to run server: %w", err)
-		panic(err)
+		os.Exit(1)
 	}
 }
