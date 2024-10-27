305121896897e52e98ed03fd026d4998796e3b96
supermarine1377 <supermarine@outlook.jp>
2024-10-27 11:43:43 +0900

    Refactor: create a handler package to seperate API handler logic from the api package

---
+++ app/internal/api/handlers/handlers_test.go
@@ -0,0 +1,22 @@
+package handlers_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/supermarine1377/todoapp/app/internal/api/handlers"
+)
+
+func TestHealthz(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, handlers.Healthz(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+	}
+}
