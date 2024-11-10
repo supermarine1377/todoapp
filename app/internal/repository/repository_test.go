5f231e969221b279227f1855aa835c8007ec46b2
supermarine1377 <supermarine@outlook.jp>
2024-11-10 12:31:27 +0900

    Add test

--- app/internal/repository/repository_test.go
+++ app/internal/repository/repository_test.go
@@ -76,3 +76,61 @@ func TestTaskRepository_ListCtx(t *testing.T) {
 		})
 	}
 }
+
+func TestTaskRepository_CreateCtx(t *testing.T) {
+	type args struct {
+		offset int
+		limit  int
+	}
+	tests := []struct {
+		name        string
+		args        args
+		prepareMock func(t *testing.T) *mock.MockDB
+		want        *task.Tasks
+		wantErr     bool
+	}{
+		{
+			name: "No error",
+			args: args{
+				offset: 0,
+				limit:  10,
+			},
+			prepareMock: func(t *testing.T) *mock.MockDB {
+				ctrl := gomock.NewController(t)
+				mockDB := mock.NewMockDB(ctrl)
+				mockDB.EXPECT().InsertCtx(
+					gomock.Any(),
+					gomock.Any(),
+				).Return(nil)
+				return mockDB
+			},
+			wantErr: false,
+		},
+		{
+			name: "Error",
+			args: args{
+				offset: 0,
+				limit:  10,
+			},
+			prepareMock: func(t *testing.T) *mock.MockDB {
+				ctrl := gomock.NewController(t)
+				mockDB := mock.NewMockDB(ctrl)
+				mockDB.EXPECT().InsertCtx(
+					gomock.Any(),
+					gomock.Any(),
+				).Return(errors.New("Any error"))
+				return mockDB
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockDB := tt.prepareMock(t)
+			tr := repository.NewTaskRepository(mockDB)
+			if err := tr.CreateCtx(context.Background(), &task.Task{}); (err != nil) != tt.wantErr {
+				t.Errorf("TaskRepository.CreateCtx() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
