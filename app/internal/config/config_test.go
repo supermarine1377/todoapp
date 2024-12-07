9807e3d52cf03aefaf5c418fa5358aac05511069
supermarine1377 <supermarine@outlook.jp>
2024-12-07 13:39:42 +0900

    Refactor tests: consolidate TestNew and improve test structure in config_test.go

--- app/internal/config/config_test.go
+++ app/internal/config/config_test.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/supermarine1377/todoapp/app/internal/config/loader"
 	config_loader "github.com/supermarine1377/todoapp/app/internal/config/loader"
 )
 
@@ -33,6 +36,52 @@ func TestConfig_Port(t *testing.T) {
 			if got := c.Port(); got != tt.want {
 				t.Errorf("Config.Port() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	type args struct {
+		clf ConfigLoaderFunc
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Config
+		wantErr bool
+	}{
+		{
+			name: "When config.ConfigLoaderFunc returns an error",
+			args: args{
+				func() (*loader.Config, error) {
+					return nil, errors.New("Some error")
+				},
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "When config.ConfigLoaderFunc returns no error",
+			args: args{
+				func() (*loader.Config, error) {
+					return &loader.Config{
+						Port: 8080,
+					}, nil
+				},
+			},
+			want: &Config{
+				config: loader.Config{
+					Port: 8080,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := New(tt.args.clf)
+			assert.Equal(t, err != nil, tt.wantErr)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
